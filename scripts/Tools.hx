6a68d14c1ddbf82d01e8dccf41189a3c2ecd46c1
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-10-24 10:31:05 +0300

    Update Tools.hx

--- scripts/Tools.hx
+++ scripts/Tools.hx
@@ -8,6 +8,6 @@ class Tools
     {
         final args:Array<String> = Sys.args();
 
-        Sys.println(ANSI.apply(args.pop(), [Red]));
+        Sys.println(ANSI.apply(args.pop(), [Red, Bold]));
     }
 }
