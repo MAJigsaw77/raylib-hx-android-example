d7d910d38746f4b4c42f1fb4209a1ac24c38dc4e
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-10-24 11:03:50 +0300

    Update Tools.hx

--- scripts/Tools.hx
+++ scripts/Tools.hx
@@ -2,12 +2,17 @@ package;
 
 import ANSI;
 
+@:nullSafety
 class Tools
 {
     public static function main():Void
     {
         final args:Array<String> = Sys.args();
+        final command:String = args.shift();
+        final runDir:String= args.pop();
 
-        Sys.println(ANSI.apply(args.toString(), [Green, Bold]));
+        Sys.println(ANSI.apply(command, [Red, Bold]));
+        Sys.println(ANSI.apply(runDir, [Blue, Underline]));
+        Sys.println(ANSI.apply(args.toString(), [Green, Underline]));
     }
 }
